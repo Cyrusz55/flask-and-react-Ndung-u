7d89cd36f778a243c91f607a2d60f2f50b65ece7
Cyrus Ndung'u Ngugi <139966564+Cyrusz55@users.noreply.github.com>
2024-08-19 23:47:44 +0300

    daily update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useEffect,useState} from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App =()=>{
+    useEffect(
+        ()=>{
+            fetch('recipe/hello')
+                .then(response=>response.json())
+                .then(data=>{console.log(data)
+                    setMessage(data.message)
+
+                })
+                .catch(err=>console.log(err))
+
+
+
+        },[]
+    )
+
+    const[message, setMessage] = useState('')
+    return(
+        <div className="app">
+            {message}
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
